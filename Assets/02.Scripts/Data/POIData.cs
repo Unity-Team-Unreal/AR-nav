2a6569b3f32b4e942a4deb613e5e606c0635de10
parkjungtae <pjtae980@gmail.com>
2024-02-17 17:49:28 +0900

    chore/POI data DB 정리
    - 기존 코드 오류 수정
    - 구글스프레드 데이터를 재빌드없이 밸런시 패칭이 가능한지 확인 중

--- Assets/02.Scripts/Data/POIData.cs
+++ Assets/02.Scripts/Data/POIData.cs
@@ -1,20 +1,74 @@
 using System;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ScriptableObjectExample", menuName = "ScriptableObject/ScriptableObjectExample")]
-public class POI : ScriptableObject //ScriptableObject를 상속
+
+public class POI : MonoBehaviour //ScriptableObject를 상속
 {
-    public ContentsData[] ARContentsData;
+    POI_Info poi_Info;
+    public ContentsData contentsData;
+
+    public void CreatPOIScriptableObject()
+    {
+        contentsData = ScriptableObject.CreateInstance<ContentsData>();
+
+    }
+    
+ 
+
+    /*private ContentsData[] FeatchData()
+    {
+        string[] columnsData = poi_Info.columnsData;
+
+        for (int i = 0; i < contentsData.Length; i++)
+        {
+            contentsData[i] = new(columnsData[0], columnsData[1], columnsData[2], columnsData[3], columnsData[4]);
+        }
+    }*/
 }
 
 [Serializable]//클래스를 직렬화하여 데이터를 저장할 수 있게 
-public class ContentsData
+public class ContentsData : ScriptableObject
 {
-    public string name;
-    public string description;
-    public string latitude;
-    public string longitude;
-    public string guide;
-    public Sprite Image;
+    [SerializeField] private string contentsname;
+    [SerializeField] private string description;
+    [SerializeField] private string latitude;
+    [SerializeField] private string longitude;
+    [SerializeField] private string guide;
+    [SerializeField] private Sprite Image;
+
+    public string Name
+    {
+        get { return contentsname; } set {  contentsname = value; }
+    }
+
+    public string Description
+    {
+        get { return description; } set { description = value; }
+    }
+
+    public string Latitude
+    {
+        get {  return latitude; } set { latitude = value;}
+    }
+
+    public string Longitude
+    {
+        get { return longitude; } set { longitude = value;}
+    }
+
+    public string Guide
+    {
+        get { return guide; } set {  guide = value; }
+    }
+
+
+    public ContentsData(string name, string description, string latitude, string longtitude, string guide)
+    {
+        this.contentsname = name;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longtitude;
+        this.guide = guide;
+    }
 }
 
--- Assets/02.Scripts/Data/POI_Info.cs
+++ Assets/02.Scripts/Data/POI_Info.cs
@@ -8,15 +8,22 @@ using UnityEngine.UIElements;
 
 public class POI_Info : MonoBehaviour
 {
+    //public POI poi;
+    //ContentsData contents;
+
     [Header("POI데이터가 있는 웹 주소")]
-    [SerializeField] string POIwebURL; // POI 데이터를 불러올 웹 주소
-    string[] rowsData;  // 행 데이터를 저장하는 배열
-    string[] columnsData;   // 열 데이터를 저장하는 배열
+    [SerializeField] string POIwebURL; // POI 데이터를 불러올 웹 
+
+    public string[] rowsData;  // 행 데이터를 저장하는 배열
+    public string[] columnsData;   // 열 데이터를 저장하는 배열
 
 
     private void Awake()
     {
         StartCoroutine(requestCoroutine()); // 코루틴 시작
+        GameObject POIDataGO = new GameObject("POIDataDB");
+        POIDataGO.transform.position = transform.position;
+        POIDataGO.transform.parent = transform;
     }
     IEnumerator requestCoroutine()
     {
@@ -43,20 +50,32 @@ public class POI_Info : MonoBehaviour
         if (WebData.isDone) // 웹 요청이 완료되었는지 확인
         {
             string json = WebData.downloadHandler.text; // 웹 요청의 결과를 문자열로 받아옴
-
             rowsData = json.Split('\n'); // 문자열을 줄바꿈('n') 기준으로 나눠서 행 데이터로 저장
+            ContentsData[] contentsdataArray = new ContentsData[rowsData.Length - 1];
 
-            for (int i = 0; i < rowsData.Length; i++)
+            for (int i = 1; i < rowsData.Length; i++)
             {
                 columnsData = rowsData[i].Split('\t'); // 행 데이터를 탭('/t') 기준으로 나눠서 열 데이터로 저장
 
-               
                 // 구글 스프레드 시트 데이터 디버그
                 foreach (var column in columnsData)
                 {
-                    Debug.Log("line: " + i + ": " + column); 
+                    Debug.Log("line: " + i + ": " + column);
                 }
             }
+
+            /*POI poi = ScriptableObject.CreateInstance<POI>();
+            poi.contentsData = contentsdataArray;
+
+            Debug.Log("ScriptableObeject created: " + poi);*/
+
+            // 스크립터블 오브젝트 내용 출력
+            //foreach (var contentsData in poi.contentsData)
+            //{
+            //    Debug.Log(contentsData);
+            //}
         }
     }
 }
+
+
