437f2da82e99cad54c1e36b92efff5618f95721e
parkjungtae <pjtae980@gmail.com>
2024-02-18 17:56:29 +0900

    feat/POI 데이터 프리팹 연동 구현
    - 구글스프레드시트에서 받아온 데이터 받아서 스크립터블 오브젝터에 적용
    - Resourece 폴더에 있는 이미지 스크립터블 오브젝터에 적용
    - 스크롤 페이지 스크립터블 오브젝트 데이터 기반 프리팹생성 구현 완료

---
+++ Assets/02.Scripts/3.AR체험존/POIPrefabs.cs
@@ -0,0 +1,40 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class POIPrefab : MonoBehaviour
+{
+    public TMP_Text contentsNameText; // UI 요소
+    public TMP_Text descriptionText; // UI 요소
+    public Image poiImage; // UI 요소
+
+    private ContentsData _contentsData; // 프리팹이 가지고 있는 데이터
+
+    public void Init(ContentsData contentsData)
+    {
+        _contentsData = contentsData;
+
+        // UI 요소에 데이터 할당
+        contentsNameText.text = _contentsData.contentsname;
+        descriptionText.text = _contentsData.description;
+        ChangeImage(poiImage, _contentsData.Image);
+    }
+
+    public void ChangeImage(Image imageComponent, Texture2D newTexture)
+    {
+        // Texture2D를 Sprite로 변환
+        Sprite newSprite = Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), new Vector2(0.5f, 0.5f));
+
+        // Image 컴포넌트의 sprite를 새로운 Sprite로 변경
+        imageComponent.sprite = newSprite;
+    }
+
+    // 프리팹 클릭 시 이벤트
+    public void OnClick()
+    {
+        Debug.Log("클릭됨");
+        //SceneManager.LoadScene("DetailScene");
+        //PlayerPrefs.SetString("SelectedPOI", JsonUtility.ToJson(_contentsData));
+    }
+}
--- Assets/02.Scripts/Data/POI_Info.cs
+++ Assets/02.Scripts/Data/POI_Info.cs
@@ -7,31 +7,51 @@ using UnityEngine.UI;
 
 public class POI_Info : MonoBehaviour
 {
-    string POIwebURL = "https://docs.google.com/spreadsheets/d/1IvrflSuhz0SyUppKPaiRQAeIQcxmbsudvWExyd3tOec/export?format=tsv&range=A2:E6"; // POI 데이터를 불러올 웹 
+    string PhotozonewebURL = "https://docs.google.com/spreadsheets/d/1IvrflSuhz0SyUppKPaiRQAeIQcxmbsudvWExyd3tOec/export?format=tsv&range=A2:E6"; // POI 데이터를 불러올 웹 
+    string DocentPOIwebURL = "https://docs.google.com/spreadsheets/d/1IvrflSuhz0SyUppKPaiRQAeIQcxmbsudvWExyd3tOec/export?format=tsv&gid=1564857787&range=A2:E6"; // POI 데이터를 불러올 웹 
+    //gid=1564857787
 
-    [Header("img데이터 주소")]
-    public Texture2D[] image;
-    //Texture2D image;
+    [Header("컨텐츠 이미지")]
+    public Texture2D[] Photozoneimage;
+    public Texture2D[] Docentimage;
+
+    [Header("컨텐츠 이미지 경로")]
+    public string Photo_floder = "Photoimage";
+    public string Docent_floder = "Docentimage";
+
 
     public POI poi;
+    public POI Docent;
+    
     public string[] rowsData;  // 행 데이터를 저장하는 배열
     public string[] columnsData;   // 열 데이터를 저장하는 배열
+
+    public GameObject contentsDataPfb; //POI 프리팹
+    public Transform PhotoscrollViewContent; //스크롤 뷰의 Contents를 가리키는 변수
+    public Transform Docentscrolltransform; //스크롤 뷰의 Contents를 가리키는 변수
 
 
     private void Awake()
     {
-        image = Resources.LoadAll<Texture2D>("Potoimage");
-        StartCoroutine(requestCoroutine()); // 코루틴 시작 
+        Photozoneimage = Resources.LoadAll<Texture2D>("Photoimage");
+        Docentimage = Resources.LoadAll<Texture2D>("Docentimage");
+
+        //poi.contentsdata = new ContentsData[.Length];
+
+        //StartCoroutine(requestCoroutine(PhotozonewebURL)); // 코루틴 시작 
+
+        StartCoroutine(requestCoroutine(poi, PhotozonewebURL, Photozoneimage, PhotoscrollViewContent)); // 코루틴 시작 
+        StartCoroutine(requestCoroutine(Docent, DocentPOIwebURL, Docentimage, Docentscrolltransform));
     }
 
 
-    IEnumerator requestCoroutine()
+    IEnumerator requestCoroutine(POI contentspoi, string link, Texture2D[] texture2D, Transform transform)
     {
-        UnityWebRequest WebData = UnityWebRequest.Get(POIwebURL); // 웹 요청 생성
+        UnityWebRequest WebData = UnityWebRequest.Get(link); // 웹 요청 생성
 
         yield return WebData.SendWebRequest(); // 웹 요청 보내기 및 응답 대기
 
-        
+
         // 웹 요층 결과에 따른 처리
         switch (WebData.result)
         {
@@ -47,19 +67,24 @@ public class POI_Info : MonoBehaviour
 
         if (WebData.isDone) // 웹 요청이 완료되었는지 확인
         {
-            POIDB(WebData.downloadHandler.text); // 웹 요청의 결과를 문자열로    
+            POIDB(WebData.downloadHandler.text, contentspoi, texture2D, transform); // 웹 요청의 결과를 문자열로    
         }
     }
 
-    void POIDB(string tsv)
+    void POIDB(string tsv, POI poi, Texture2D[] texture2D, Transform transform)
     {
         rowsData = tsv.Split('\n'); // 문자열을 줄바꿈('n') 기준으로 나눠서 행 데이터로 저장
         int rowsSize = rowsData.Length;
         int columnSize = rowsData[0].Split("\t").Length;
 
+        
+
         for (int i = 0; i < rowsSize; i++)
         {
             columnsData = rowsData[i].Split('\t'); // 행 데이터를 탭('/t') 기준으로 나눠서 열 데이터로 저장
+
+            GameObject poiPrefabsIntance = Instantiate(contentsDataPfb, transform);
+            
 
             for (int j = 0; j < columnSize; j++)
             {
@@ -70,8 +95,10 @@ public class POI_Info : MonoBehaviour
                 contentsData.latitude = columnsData[2];
                 contentsData.longitude = columnsData[3];
                 contentsData.guide = columnsData[4];
-                contentsData.Image = image[i];
+                contentsData.Image = texture2D[i];
             }
+
+            poiPrefabsIntance.GetComponent<POIPrefab>().Init(poi.contentsdata[i]);
 
             // 구글 스프레드 시트 데이터 디버그
             foreach (var column in columnsData)
