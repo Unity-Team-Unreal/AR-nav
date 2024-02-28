710caeb4aed01af6a880b248aaf29c2caba1c448
KimHyoJin <hyojin1537@outlook.com>
2024-02-28 09:33:07 +0900

    02/28
    터치 추가

--- Assets/02.Scripts/KimHyoJin/Maps/MapTransformManager.cs
+++ Assets/02.Scripts/KimHyoJin/Maps/MapTransformManager.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 
 public class MapTransformManager : MonoBehaviour
 {
+    [Header("지도의 최소, 최대 확대 비율")]
     [SerializeField] float minSize;
     [SerializeField] float maxSize;
+    [Header("줌인,아웃, 스크롤링 속도")]
     [SerializeField] float ZoomSpeed;
     [SerializeField] float MoveSpeed;
 
@@ -15,8 +17,10 @@ public class MapTransformManager : MonoBehaviour
 
     Vector3 mapPosition;
 
-    Touch touch;
-    Vector3 touchedPos;
+    Vector2 nowPos, prePos;
+    Vector3 movePos;
+
+
     void Start()
     {
         MapImage = GetComponent<RawImage>();
@@ -27,8 +31,8 @@ public class MapTransformManager : MonoBehaviour
 
     void Update()
     {
-        MapZoomInOut();
-        MapMove();
+        TouchZoom();
+        TouchMove();
     }
 
     void MapZoomInOut()
@@ -47,17 +51,35 @@ public class MapTransformManager : MonoBehaviour
 
 
 
-    void TouchMoveTest()
+    void TouchZoom()
     {
-        if(Input.touchCount>0)
-            for(int i = 0; i < Input.touchCount; i++)
-            {
-                Touch tempTouchs = Input.GetTouch(i);
-                if(tempTouchs.phase == TouchPhase.Began)
-                {
-                    touchedPos = Camera.main.ScreenToWorldPoint(tempTouchs.position);
-                }
-            }
+        if (Input.touchCount == 2) //손가락 2개가 눌렸을 때
+        {
+            Touch touchZero = Input.GetTouch(0); //첫번째 손가락 터치를 저장
+            Touch touchOne = Input.GetTouch(1); //두번째 손가락 터치를 저장
+
+            //터치에 대한 이전 위치값을 각각 저장함
+            //처음 터치한 위치(touchZero.position)에서 이전 프레임에서의 터치 위치와 이번 프로임에서 터치 위치의 차이를 뺌
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition; //deltaPosition는 이동방향 추적할 때 사용
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // 각 프레임에서 터치 사이의 벡터 거리 구함
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude; //magnitude는 두 점간의 거리 비교(벡터)
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // 거리 차이 구함(거리가 이전보다 크면(마이너스가 나오면)손가락을 벌린 상태_줌인 상태)
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            if (deltaMagnitudeDiff > 0) MapImage.transform.localScale *= 1f + (ZoomSpeed * Time.deltaTime);
+
+            else if (deltaMagnitudeDiff<0)  MapImage.transform.localScale /= 1f + (ZoomSpeed * Time.deltaTime);
+
+            float MapScaleX = Mathf.Clamp(MapImage.transform.localScale.x, minSize, maxSize);    //지도의 확대 레벨을 사이로 제한
+
+            float MapScaleY = Mathf.Clamp(MapImage.transform.localScale.y, minSize, maxSize);    //지도의 확대 레벨을 사이로 제한
+
+            MapImage.transform.localScale = new Vector2(MapScaleX, MapScaleY);
+        }
     }
 
 
@@ -74,14 +96,36 @@ public class MapTransformManager : MonoBehaviour
 
         transform.position += mapPosition * Time.deltaTime * MoveSpeed * MapImage.transform.localScale.x;
 
-        float x = (Screen.width / 2) * (transform.localScale.x-1);
-        float y = (Screen.height / 2) * (transform.localScale.y - 1);
 
-        float MapPositionX = Mathf.Clamp(MapImage.rectTransform.anchoredPosition.x, -x, x);    //지도의 확대 레벨을 사이로 제한
+    }
 
-        float MapPositionY = Mathf.Clamp(MapImage.rectTransform.anchoredPosition.y, -y, y);    //지도의 확대 레벨을 사이로 제한
+    void TouchMove()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                prePos = touch.position - touch.deltaPosition;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                nowPos = touch.position - touch.deltaPosition;
+                movePos = (Vector3)(prePos - nowPos) * Time.deltaTime * MoveSpeed * MapImage.transform.localScale.x;
+                MapImage.transform.Translate(movePos);
 
-        MapImage.rectTransform.anchoredPosition = new Vector2(MapPositionX, MapPositionY);
+                float x = (Screen.width / 2) * (transform.localScale.x - 1);
+                float y = (Screen.height / 2) * (transform.localScale.y - 1);
+
+                float MapPositionX = Mathf.Clamp(MapImage.rectTransform.anchoredPosition.x, -x, x);    //지도의 확대 레벨을 사이로 제한
+
+                float MapPositionY = Mathf.Clamp(MapImage.rectTransform.anchoredPosition.y, -y, y);    //지도의 확대 레벨을 사이로 제한
+
+                MapImage.rectTransform.anchoredPosition = new Vector2(MapPositionX, MapPositionY);
+
+                prePos = touch.position - touch.deltaPosition;
+            }
+        }
 
     }
 }
